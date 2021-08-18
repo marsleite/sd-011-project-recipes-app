3b41fde835e06ee24f457cafa97d93caaa8956a5
DiegoCS777 <diegomethal@gmail.com>
2021-08-18 16:46:38 -0300

    cria teste da tela de comidas

---
+++ src/tests/Bebidas.test.js
@@ -0,0 +1,69 @@
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import Bebidas from '../pages/Bebidas';
+import renderWithRouterAndRedux from './renderWithRouterAndRedux';
+
+const drinksAmount = 12;
+const btnDrinkId = 'Ordinary Drink-category-filter';
+const btnAll = 'All-category-filter';
+
+describe('tests drinks page ', () => {
+  test('tests route', async () => {
+    const { history, getByTestId } = renderWithRouterAndRedux(<Bebidas />);
+    history.push('/bebidas');
+    const allButton = await getByTestId(btnAll);
+    userEvent.click(allButton);
+    const path = history.location.pathname;
+
+    expect(path).toBe('/bebidas');
+  });
+
+  test('tests drink cards quantity', async () => {
+    const {
+      history, findByText, findAllByTestId } = renderWithRouterAndRedux(<Bebidas />);
+    history.push('/bebidas');
+
+    const drink = await findByText(/B-53/i);
+    expect(drink).toBeInTheDocument();
+
+    const cardDrinks = await findAllByTestId(/recipe-card/i);
+    expect(cardDrinks.length).toBe(drinksAmount);
+
+    cardDrinks.forEach((drinkCard) => {
+      expect(drinkCard).toBeInTheDocument();
+    });
+  });
+
+  test('tests filter by categories buttons render', async () => {
+    const { history, findByTestId } = renderWithRouterAndRedux(<Bebidas />);
+    history.push('/bebidas');
+
+    const buttonAll = await findByTestId(btnAll);
+    expect(buttonAll).toBeInTheDocument(btnAll);
+
+    const btnOrdinary = await findByTestId(btnDrinkId);
+    expect(btnOrdinary).toBeInTheDocument(btnDrinkId);
+
+    const btnCocktail = await findByTestId('Cocktail-category-filter');
+    expect(btnCocktail).toBeInTheDocument('Cocktail-category-filter');
+
+    const btnMilk = await findByTestId('Milk / Float / Shake-category-filter');
+    expect(btnMilk).toBeInTheDocument('Milk / Float / Shake-category-filter');
+
+    const btnOther = await findByTestId('Other/Unknown-category-filter');
+    expect(btnOther).toBeInTheDocument('Other/Unknown-category-filter');
+
+    const btnCocoa = await findByTestId('Cocoa-category-filter');
+    expect(btnCocoa).toBeInTheDocument('Cocoa-category-filter');
+  });
+
+  test('tests clicking on a category button', async () => {
+    const { history, findByTestId, findByText } = renderWithRouterAndRedux(<Bebidas />);
+    history.push('/bebidas');
+
+    const btnOrdinary = await findByTestId(btnDrinkId);
+    userEvent.click(btnOrdinary);
+    const text = await findByText('A Day at the Beach');
+    expect(text).toBeInTheDocument();
+  });
+});
