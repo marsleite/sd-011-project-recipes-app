7bbe50b2a95be2c008f22f06a08ba578788583c9
LeoFuna <leo.funa@gmail.com>
2021-08-19 15:23:44 -0300

    CSS comidas favoritas

--- src/pages/ReceitasFavoritas.jsx
+++ src/pages/ReceitasFavoritas.jsx
@@ -76,80 +76,95 @@ export default class ReceitasFavoritas extends Component {
     return itemsToRender.map((item, index) => {
       if (item.type === 'comida') {
         return (
-          <div key={ item.id }>
+          <div className="recipe-card" style={ { margin: '20px auto', width: '80vw' } } key={ item.id }>
             <div
               onClick={ () => this.redirectToRecipeDetails(item.id, 'comidas') }
               onKeyDown={ () => this.redirectToRecipeDetails(item.id, 'comidas') }
               role="button"
               tabIndex="0"
             >
-              <h3 data-testid={ `${index}-horizontal-name` }>{item.name}</h3>
+              <h3 style={ { fontWeight: 'bolder' } } data-testid={ `${index}-horizontal-name` }>{item.name}</h3>
               <img
                 data-testid={ `${index}-horizontal-image` }
                 src={ item.image }
                 alt="imagem comida"
-                style={ { width: '50px' } }
+                className="recipe-image"
               />
             </div>
-            <p data-testid={ `${index}-horizontal-top-text` }>
+            <p style={ { fontSize: '21px' } } data-testid={ `${index}-horizontal-top-text` }>
               { item.area }
               {' '}
               -
               {' '}
               { item.category }
             </p>
-            <button
-              onClick={ () => this.shareLinkClick(item.id, 'comidas') }
-              type="button"
-            >
-              <img
+            <div style={ { display: 'flex', alignItems: 'center' } }>
+              <button
+                onClick={ () => this.shareLinkClick(item.id, 'comidas') }
+                type="button"
+                className="share-button"
+                style={ { backgroundColor: 'inherit' } }
+              >
+                {/* <img
                 data-testid={ `${index}-horizontal-share-btn` }
                 src={ shareIcon }
                 alt="share"
-              />
-            </button>
-            <button onClick={ () => this.removeFavorite(item.id) } type="button">
-              <img
+              /> */}
+                <i className="fas fa-share-alt-square" />
+              </button>
+              <button onClick={ () => this.removeFavorite(item.id) } className="like-button" style={ { padding: '2px 8px', height: '38.5px', display: 'flex', alignItems: 'center' } } type="button">
+                {/* <img
                 data-testid={ `${index}-horizontal-favorite-btn` }
                 src={ blackHeartIcon }
                 alt="White Heart"
-              />
-            </button>
-            {shareButton ? <span style={ { color: 'red' } }>Link copiado!</span> : null}
+              /> */}
+                <i className="fas fa-heart like-button-liked" style={ { fontSize: '21px' } } />
+              </button>
+              {shareButton ? <span style={ { color: 'red', fontSize: '16px' } }>Link copiado!</span> : null}
+            </div>
           </div>);
       }
       return (
-        <div key={ item.id }>
+        <div className="recipe-card" style={ { margin: '20px auto', width: '80vw' } } key={ item.id }>
           <div
             onClick={ () => this.redirectToRecipeDetails(item.id, 'bebidas') }
             onKeyDown={ () => this.redirectToRecipeDetails(item.id, 'bebidas') }
             role="button"
             tabIndex="0"
           >
-            <h3 data-testid={ `${index}-horizontal-name` }>{item.name}</h3>
+            <h3 style={ { fontWeight: 'bolder' } } data-testid={ `${index}-horizontal-name` }>{item.name}</h3>
             <img
               data-testid={ `${index}-horizontal-image` }
               src={ item.image }
               alt="imagem bebida"
-              style={ { width: '50px' } }
+              className="recipe-image"
             />
           </div>
-          <p data-testid={ `${index}-horizontal-top-text` }>{ item.alcoholicOrNot }</p>
-          <button onClick={ () => this.shareLinkClick(item.id, 'bebidas') } type="button">
-            <img
+          <p style={ { fontSize: '21px' } } data-testid={ `${index}-horizontal-top-text` }>{ item.alcoholicOrNot }</p>
+          <div style={ { display: 'flex', alignItems: 'center' } }>
+            <button
+              onClick={ () => this.shareLinkClick(item.id, 'bebidas') }
+              className="share-button"
+              style={ { backgroundColor: 'inherit' } }
+              type="button"
+            >
+              {/* <img
               data-testid={ `${index}-horizontal-share-btn` }
               src={ shareIcon }
               alt="share"
-            />
-          </button>
-          <button onClick={ () => this.removeFavorite(item.id) } type="button">
-            <img
+            /> */}
+              <i className="fas fa-share-alt-square" />
+            </button>
+            <button onClick={ () => this.removeFavorite(item.id) } style={ { padding: '2px 8px', height: '38.5px', display: 'flex', alignItems: 'center' } } className="like-button" type="button">
+              {/* <img
               data-testid={ `${index}-horizontal-favorite-btn` }
               src={ blackHeartIcon }
               alt="White Heart"
-            />
-          </button>
-          {shareButton ? <span style={ { color: 'red' } }>Link copiado!</span> : null}
+            /> */}
+              <i className="fas fa-heart like-button-liked" style={ { fontSize: '21px' } } />
+            </button>
+            {shareButton ? <span style={ { color: 'red', fontSize: '16px' } }>Link copiado!</span> : null}
+          </div>
         </div>);
     });
   }
