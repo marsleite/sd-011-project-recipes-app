3112049b27eeaa10a92da80169c63cf43f66e784
nandovbr <nandovbr15@hotmail.com>
2021-08-18 21:02:04 -0300

    lint adjusts

--- src/pages/RecipeProgressDrink.js
+++ src/pages/RecipeProgressDrink.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
 import { searchDrinkById } from '../services/RequestDrinks';
 import '../styles/drink.css';
 
