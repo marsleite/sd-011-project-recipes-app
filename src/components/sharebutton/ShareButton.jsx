1658e0f648decba3771a3bf46eda3c99d105b755
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-08-19 06:46:33 -0300

    Requisito 43 concluido e testando evaluator

--- src/components/sharebutton/ShareButton.jsx
+++ src/components/sharebutton/ShareButton.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import copy from 'clipboard-copy';
+import { useLocation } from 'react-router';
 
 import ShareIcon from '../../images/shareIcon.svg';
 
 export default function ShareButton() {
+  const location = useLocation();
+  const [isHidden, setIsHidden] = useState(true);
+
+  function handleClipboardCopy() {
+    copy(`http://localhost:3000${location.pathname}`);
+    setIsHidden(false);
+  }
+
   return (
     <div data-testid="share-btn">
-      <button type="button" src={ ShareIcon }>
+      <button
+        type="button"
+        src={ ShareIcon }
+        onClick={ () => handleClipboardCopy() }
+      >
         <img src={ ShareIcon } alt="Botão de compartilhar" />
       </button>
+      <div hidden={ isHidden }>
+        <strong>Link copiado!</strong>
+        <button type="button" onClick={ () => setIsHidden(true) }>x</button>
+      </div>
     </div>
   );
 }
